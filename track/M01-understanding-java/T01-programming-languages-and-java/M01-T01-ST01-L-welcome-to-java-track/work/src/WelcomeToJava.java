6f379664b7cb3b2106d271c7c7db76e214bd40c2
Nishant <nishant594276@gmail.com>
2026-07-30 18:47:17 +0530

    two line change

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("Welcome to java track");
+        System.out.println("Welcome to java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now i am learning how to express that logic using java");
     }
 }
