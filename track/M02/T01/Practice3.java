2f8d3d44a687457c91e02f5945d10a412b4546f7
Nishant <nishant594276@gmail.com>
2026-08-03 15:52:30 +0530

    module 2 topic 1 practice 3

---
+++ track/M02/T01/Practice3.java
@@ -0,0 +1,10 @@
+
+public class Practice3 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHourse = 3;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice " + dailyPracticeHourse + "hours");
+    }
+}
