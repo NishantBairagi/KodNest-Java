96f8eab17aeeba3466918185de1223fefd974786
Nishant <nishant594276@gmail.com>
2026-08-11 15:34:42 +0530

    Module02 T05 P01

---
+++ track/M02/T05/Practice1.java
@@ -0,0 +1,15 @@
+
+class Robot {
+
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+class Practice1 {
+
+    public static void main(String[] args) {
+        Robot r = new Robot();
+        r.speak();
+    }
+}
