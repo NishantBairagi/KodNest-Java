ccf0fa6ce5a7f84a55e62909973d2eed25e424b6
Nishant <nishant594276@gmail.com>
2026-08-11 15:48:06 +0530

    Module02 T05 Method type-4

--- track/M02/T05/Method.java
+++ track/M02/T05/Method.java
@@ -6,6 +6,8 @@ class Method {
         add(2, 3);
         int res = add2();
         System.out.println(res);
+        int res2 = add3(5, 5);
+        System.out.println(res2);
     }
 
     static void greet() {
@@ -20,6 +22,10 @@ class Method {
     static int add2() {
         int a = 10;
         int b = 20;
+        return a + b;
+    }
+
+    static int add3(int a, int b) {
         return a + b;
     }
 }
