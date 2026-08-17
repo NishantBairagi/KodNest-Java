da099ca3b4f28e68b3abe6d63edbdb4a5af891ea
Nishant <nishant594276@gmail.com>
2026-08-17 16:54:38 +0530

    Module 2 Topic 7 practice 3

---
+++ track/M02/T07/Practice3.java
@@ -0,0 +1,12 @@
+
+class Practice3 {
+
+    public static void main(String[] args) {
+        int[] values = {10, 20, 30, 40, 50};
+        int bonus = 5;
+        System.out.print("Updated values: ");
+        for (int x : values) {
+            System.out.print((x + bonus) + " ");
+        }
+    }
+}
