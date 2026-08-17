266a669067029362315efb1f896c08bb69e65293
Nishant <nishant594276@gmail.com>
2026-08-17 15:40:34 +0530

    Module 2 Topic 7 program 2

---
+++ track/M02/T07/Pgm2.java
@@ -0,0 +1,11 @@
+
+class Pgm2 {
+
+    public static void main(String[] args) {
+        int[] ar = {10, 20, 30, 40, 50};
+        System.out.println("Array Elements are");
+        for (int x : ar) {
+            System.out.println(x);
+        }
+    }
+}
