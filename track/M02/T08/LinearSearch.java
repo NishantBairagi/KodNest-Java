302f92720b25867b080f4b5f20d513d6ac63d916
Nishant <nishant594276@gmail.com>
2026-08-18 15:47:09 +0530

    Module 2 Topic 8 searching element in array

---
+++ track/M02/T08/LinearSearch.java
@@ -0,0 +1,24 @@
+
+class LinearSearch {
+
+    public static void ArraySearch(int[] arr, int key) {
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element Found at index: " + i);
+                found = true;
+                break;
+
+            }
+        }
+        if (!found) {
+            System.out.println("Element is not present in this Array");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+        ArraySearch(arr, key);
+    }
+}
