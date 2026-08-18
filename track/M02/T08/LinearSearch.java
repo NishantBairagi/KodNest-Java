763aad6c423f3ac9b4c88977bf2003858e379ab3
Nishant <nishant594276@gmail.com>
2026-08-18 16:36:07 +0530

    Module 2 Topic 8 frequency and duplicates in an array

--- track/M02/T08/LinearSearch.java
+++ track/M02/T08/LinearSearch.java
@@ -26,8 +26,29 @@ class LinearSearch {
         return rev;
     }
 
+    public static int freq(int[] arr, int key) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean findDuplicates(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
+        int[] arr = {10, 20, 30, 40, 10, 50};
         int key = 30;
         arraySearch(arr, key);
         int arr2[] = reverseArray(arr);
@@ -35,5 +56,10 @@ class LinearSearch {
         for (int x : arr2) {
             System.out.print(x + " ");
         }
+        System.out.println();
+        int freqOfKey = freq(arr, key);
+        System.out.println("frequency of key in array: " + freqOfKey);
+        boolean duplicate = findDuplicates(arr);
+        System.out.println(duplicate ? "Duplicate are present" : "Duplicates are not present");
     }
 }
