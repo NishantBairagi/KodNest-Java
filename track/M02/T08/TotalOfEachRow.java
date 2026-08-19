4b946751c4d5b0cd5da5e7f57d4fb4b4b86c87b9
Nishant <nishant594276@gmail.com>
2026-08-19 16:43:51 +0530

    Total sum of each row in 2D  Array

---
+++ track/M02/T08/TotalOfEachRow.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+class TotalOfEachRow {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+
+        }
+        sc.close();
+        for (int i = 0; i < r; i++) {
+            int total = 0;
+            for (int j = 0; j < c; j++) {
+                total += matrix[i][j];
+            }
+            System.out.println("Row " + (i + 1) + " total: " + total);
+        }
+    }
+}
