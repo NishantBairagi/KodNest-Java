718fa860721f5d21030ba6b05b49afb71c3c4616
Nishant <nishant594276@gmail.com>
2026-09-07 19:40:08 +0530

    traversing string

---
+++ track/M04/T01/Pgm5.java
@@ -0,0 +1,12 @@
+
+class Pgm5 {
+
+    public static void main(String[] args) {
+        String s1 = "Raja";
+        System.out.println(s1.length());
+        for (int i = 0; i < s1.length(); i++) {
+            System.out.println(s1.charAt(i));
+        }
+
+    }
+}
