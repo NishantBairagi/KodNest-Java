ccde6369c3c56c6638ff4686217568038bcb9bcc
Nishant <nishant594276@gmail.com>
2026-09-07 19:30:33 +0530

    == method with string

---
+++ track/M04/T01/Pgm1.java
@@ -0,0 +1,13 @@
+
+class Pgm1 {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+        if (s1 == s2) {
+            System.out.println("Ref are equals");
+        } else {
+            System.out.println("Ref are not equals");
+        }
+    }
+}
