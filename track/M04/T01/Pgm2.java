2b95007a27ea5df85801c1311261cdf461b5f303
Nishant <nishant594276@gmail.com>
2026-09-07 19:34:29 +0530

    == method with string new object

---
+++ track/M04/T01/Pgm2.java
@@ -0,0 +1,13 @@
+
+class Pgm2 {
+
+    public static void main(String[] args) {
+        String s1 = new String("java");
+        String s2 = new String("java");
+        if (s1 == s2) {
+            System.out.println("ref are equals");
+        } else {
+            System.out.println("ref are not equals");
+        }
+    }
+}
