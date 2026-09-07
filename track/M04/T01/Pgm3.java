c5c4b108e97cd348c7526a15724b6df1d4b9d475
Nishant <nishant594276@gmail.com>
2026-09-07 19:36:17 +0530

    .equals() method with string compare the content

---
+++ track/M04/T01/Pgm3.java
@@ -0,0 +1,13 @@
+
+class Pgm3 {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+        if (s1.equals(s2)) {
+            System.out.println("Ref are equals");
+        } else {
+            System.out.println("Ref are not equals");
+        }
+    }
+}
