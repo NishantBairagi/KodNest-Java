cafe4366566757ea0e44c26b92315d5d606b4a19
Nishant <nishant594276@gmail.com>
2026-09-08 19:05:44 +0530

    .equalsIgnoreCase() method

---
+++ track/M04/T01/Pgm7.java
@@ -0,0 +1,13 @@
+
+class Pgm7 {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "javA";
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("Ref are equals");
+        } else {
+            System.out.println("Ref are not equals");
+        }
+    }
+}
