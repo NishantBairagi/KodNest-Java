dbf3f2245dd3f4f5b1d46f85b50c882196c1138e
Nishant <nishant594276@gmail.com>
2026-09-09 19:12:07 +0530

    M04 T01 P04

---
+++ track/M04/T01/Practice4.java
@@ -0,0 +1,11 @@
+
+class Practice4 {
+
+    public static void main(String[] args) {
+        String s1 = "Raja";
+        System.out.println("before : " + s1);
+        String res = s1.toUpperCase();
+        System.out.println("After operation: " + s1);
+        System.out.println(res);
+    }
+}
