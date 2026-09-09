e18426f509e0de87dec9167a00cac1d6b172da0c
Nishant <nishant594276@gmail.com>
2026-09-09 19:15:05 +0530

    .concat() method

---
+++ track/M04/T01/Pgm8.java
@@ -0,0 +1,16 @@
+
+class Pgm8 {
+
+    public static void main(String[] args) {
+        String s1 = "Anu";
+        String s2 = "Banu";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+
+        String s4 = new String("Raja");
+        String s5 = new String("Rani");
+
+        System.out.println(s4.concat(s5).concat("Ramu"));
+
+    }
+}
