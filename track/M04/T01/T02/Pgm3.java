58050eba46af54fe15b65f7c94c8d7d27517b2f3
Nishant <nishant594276@gmail.com>
2026-09-10 19:32:05 +0530

    String builder toString() method

---
+++ track/M04/T01/T02/Pgm3.java
@@ -0,0 +1,9 @@
+
+class Pgm3 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Raja");
+        String res = sb.toString();
+        System.out.println(res);
+    }
+}
